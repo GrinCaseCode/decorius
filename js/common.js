6b92ded0f0c4ae8906fb486879ba41eee8874082
nickolay.grin <grinmail84@gmail.com>
2021-04-19 12:29:38 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -6,6 +6,12 @@ $(".change-color li").click(function(e) {
 	$(this).siblings("li").removeClass("active");
 });
 
+
+	//плавный скролл
+	$(".link-simple_features").mPageScroll2id();
+
+$('select').styler();
+
 		//кнопка sandwich
 		$(".btn_nav").click(function() {
 			$(".sandwich").toggleClass("active");
@@ -16,8 +22,43 @@ $(".change-color li").click(function(e) {
 			}
 		});
 
-	
+		$(".btn-view_list").click(function(e) {
+			e.preventDefault();
+			$(this).addClass("active");
+			$(".btn-view_grid").removeClass("active");
+			$(".row_products > div").removeClass("col-lg-4 col-6");
+			$(".row_products > div").addClass("col-12");
+			$(".item-card").addClass("row");
+			$(".item-card > div").addClass("col-md-4");
+		});
 
+		$(".btn-view_grid").click(function(e) {
+			e.preventDefault();
+			$(this).addClass("active");
+			$(".btn-view_list").removeClass("active");
+			$(".row_products > div").addClass("col-lg-4 col-6");
+			$(".row_products > div").removeClass("col-12");
+			$(".item-card").removeClass("row");
+			$(".item-card > div").removeClass("col-md-4");
+			
+		});
+
+		$(".title-filter").click(function() {
+			$(this).parent().toggleClass("active");
+			$(".sidebar-filter__content").slideToggle(200);
+		});
+
+
+		$(".unit-checkbox .link-simple").click(function(e) {
+			e.preventDefault();
+			if ($(this).siblings(".checkbox_hidden").is(":hidden")) {
+				$(this).siblings(".checkbox_hidden").slideDown(200);
+				$(this).html("Скрыть");
+			} else {
+				$(this).siblings(".checkbox_hidden").slideUp(200);
+				$(this).html("Показать все");
+			}
+		});
 
 		$(".billbord").waypoint(function(direction) {
 			if (direction === "down") {
@@ -220,7 +261,41 @@ new Promise((resolve, reject) => {
  	]
  });
 
-  $('.blogs .row_blogs').slick({
+ $('.tabs').slick({
+ 	dots: false,
+ 	arrows: false,
+ 	slidesToShow:1,
+ 	variableWidth: true,
+ 	infinite: false,
+ 	arrows: false,
+ 	slidesToScroll: 1,
+ 	mobileFirst: true,
+ 	responsive: [
+ 	{
+ 		breakpoint: 768,
+ 		settings: 'unslick'
+ 	}
+ 	]
+ });
+
+  $('.row_reviews').slick({
+ 	dots: false,
+ 	arrows: false,
+ 	slidesToShow:1,
+ 	variableWidth: true,
+ 	infinite: false,
+ 	arrows: false,
+ 	slidesToScroll: 1,
+ 	mobileFirst: true,
+ 	responsive: [
+ 	{
+ 		breakpoint: 480,
+ 		settings: 'unslick'
+ 	}
+ 	]
+ });
+
+ $('.blogs .row_blogs').slick({
  	dots: false,
  	arrows: false,
  	slidesToShow:1,
@@ -285,7 +360,7 @@ new Promise((resolve, reject) => {
 
  });
 
-  $('.slider-products-page').slick({
+ $('.slider-products-page').slick({
  	arrows: true,
  	dots: true,
  	infinite: true,
@@ -447,51 +522,51 @@ new Promise((resolve, reject) => {
  });
 
  
-var $range = $(".range-catalog__input"),
-	$from = $(".control-input__from"),
-	$to = $(".control-input__to"),
-        min = 0,
-        max = 40000;
-    $range.ionRangeSlider({
-        type: "double",
-        min: min,
-        max: max,
-        from: 1000,
-        to: 18000,
-        prettify_enabled: true,
-        onChange: function(data) {
-            updateValues()
-        }
-    });
+ var $range = $(".range-catalog__input"),
+ $from = $(".control-input__from"),
+ $to = $(".control-input__to"),
+ min = 0,
+ max = 40000;
+ $range.ionRangeSlider({
+ 	type: "double",
+ 	min: min,
+ 	max: max,
+ 	from: 1000,
+ 	to: 18000,
+ 	prettify_enabled: true,
+ 	onChange: function(data) {
+ 		updateValues()
+ 	}
+ });
  
-    function number_format(num, format) {
-        num = (num + "").replace(/(\s)+/g, "");
-        return format ? num.replace(/(\d{1,3})(?=(?:\d{3})+$)/g, "$1 ") : num
-    }
-    $range = $range.data("ionRangeSlider");
-    var updateValues = function() {
-        var res = $range.result;
-        $from.val(number_format(res.from, true) + " ₽");
-        $to.val(number_format(res.to,true) + " ₽")
-    };
-    $from.on("focus", function() {
-        this.value = number_format(this.value);
-        this.focus();
-        this.selectionStart = this.value.length
-    }).on("input", function() {
-        $range.update({
-            from: this.value
-        })
-    }).on("blur", updateValues);
-    $to.on("focus", function() {
-        this.value = number_format(this.value);
-        this.focus();
-        this.selectionStart = this.value.length
-    }).on("input", function() {
-        $range.update({
-            to: this.value
-        })
-    }).on("blur", updateValues)
+ function number_format(num, format) {
+ 	num = (num + "").replace(/(\s)+/g, "");
+ 	return format ? num.replace(/(\d{1,3})(?=(?:\d{3})+$)/g, "$1 ") : num
+ }
+ $range = $range.data("ionRangeSlider");
+ var updateValues = function() {
+ 	var res = $range.result;
+ 	$from.val(number_format(res.from, true) + " ₽");
+ 	$to.val(number_format(res.to,true) + " ₽")
+ };
+ $from.on("focus", function() {
+ 	this.value = number_format(this.value);
+ 	this.focus();
+ 	this.selectionStart = this.value.length
+ }).on("input", function() {
+ 	$range.update({
+ 		from: this.value
+ 	})
+ }).on("blur", updateValues);
+ $to.on("focus", function() {
+ 	this.value = number_format(this.value);
+ 	this.focus();
+ 	this.selectionStart = this.value.length
+ }).on("input", function() {
+ 	$range.update({
+ 		to: this.value
+ 	})
+ }).on("blur", updateValues)
  
 
  var objectFitImages=function(){"use strict";function t(t,e){return"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='"+t+"' height='"+e+"'%3E%3C/svg%3E"}function e(t){if(t.srcset&&!p&&window.picturefill){var e=window.picturefill._;t[e.ns]&&t[e.ns].evaled||e.fillImg(t,{reselect:!0}),t[e.ns].curSrc||(t[e.ns].supported=!1,e.fillImg(t,{reselect:!0})),t.currentSrc=t[e.ns].curSrc||t.src}}function i(t){for(var e,i=getComputedStyle(t).fontFamily,r={};null!==(e=u.exec(i));)r[e[1]]=e[2];return r}function r(e,i,r){var n=t(i||1,r||0);b.call(e,"src")!==n&&h.call(e,"src",n)}function n(t,e){t.naturalWidth?e(t):setTimeout(n,100,t,e)}function c(t){var c=i(t),o=t[l];if(c["object-fit"]=c["object-fit"]||"fill",!o.img){if("fill"===c["object-fit"])return;if(!o.skipTest&&f&&!c["object-position"])return}if(!o.img){o.img=new Image(t.width,t.height),o.img.srcset=b.call(t,"data-ofi-srcset")||t.srcset,o.img.src=b.call(t,"data-ofi-src")||t.src,h.call(t,"data-ofi-src",t.src),t.srcset&&h.call(t,"data-ofi-srcset",t.srcset),r(t,t.naturalWidth||t.width,t.naturalHeight||t.height),t.srcset&&(t.srcset="");try{s(t)}catch(t){window.console&&console.warn("https://bit.ly/ofi-old-browser")}}e(o.img),t.style.backgroundImage='url("'+(o.img.currentSrc||o.img.src).replace(/"/g,'\\"')+'")',t.style.backgroundPosition=c["object-position"]||"center",t.style.backgroundRepeat="no-repeat",t.style.backgroundOrigin="content-box",/scale-down/.test(c["object-fit"])?n(o.img,function(){o.img.naturalWidth>t.width||o.img.naturalHeight>t.height?t.style.backgroundSize="contain":t.style.backgroundSize="auto"}):t.style.backgroundSize=c["object-fit"].replace("none","auto").replace("fill","100% 100%"),n(o.img,function(e){r(t,e.naturalWidth,e.naturalHeight)})}function s(t){var e={get:function(e){return t[l].img[e?e:"src"]},set:function(e,i){return t[l].img[i?i:"src"]=e,h.call(t,"data-ofi-"+i,e),c(t),e}};Object.defineProperty(t,"src",e),Object.defineProperty(t,"currentSrc",{get:function(){return e.get("currentSrc")}}),Object.defineProperty(t,"srcset",{get:function(){return e.get("srcset")},set:function(t){return e.set(t,"srcset")}})}function o(){function t(t,e){return t[l]&&t[l].img&&("src"===e||"srcset"===e)?t[l].img:t}d||(HTMLImageElement.prototype.getAttribute=function(e){return b.call(t(this,e),e)},HTMLImageElement.prototype.setAttribute=function(e,i){return h.call(t(this,e),e,String(i))})}function a(t,e){var i=!y&&!t;if(e=e||{},t=t||"img",d&&!e.skipTest||!m)return!1;"img"===t?t=document.getElementsByTagName("img"):"string"==typeof t?t=document.querySelectorAll(t):"length"in t||(t=[t]);for(var r=0;r<t.length;r++)t[r][l]=t[r][l]||{skipTest:e.skipTest},c(t[r]);i&&(document.body.addEventListener("load",function(t){"IMG"===t.target.tagName&&a(t.target,{skipTest:e.skipTest})},!0),y=!0,t="img"),e.watchMQ&&window.addEventListener("resize",a.bind(null,t,{skipTest:e.skipTest}))}var l="fregante:object-fit-images",u=/(object-fit|object-position)\s*:\s*([-.\w\s%]+)/g,g="undefined"==typeof Image?{style:{"object-position":1}}:new Image,f="object-fit"in g.style,d="object-position"in g.style,m="background-size"in g.style,p="string"==typeof g.currentSrc,b=g.getAttribute,h=g.setAttribute,y=!1;return a.supportsObjectFit=f,a.supportsObjectPosition=d,o(),a}();
